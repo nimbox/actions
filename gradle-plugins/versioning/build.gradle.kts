2a95e5f88a837a3645730610e68b412cfdf39d0d
Ricardo Marimón <rmarimon@nimbox.com>
2025-11-17 19:06:17 -0400

    Setup publishing

--- gradle-plugins/versioning/build.gradle.kts
+++ gradle-plugins/versioning/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
 	id("java-gradle-plugin")
+	id("maven-publish")
 }
 
 gradlePlugin {
@@ -7,6 +8,19 @@ gradlePlugin {
 		create("versioning") {
 			id = "com.nimbox.tools.versioning"
 			implementationClass = "com.nimbox.tools.versioning.VersioningPlugin"
+		}
+	}
+}
+
+publishing {
+	repositories {
+		maven {
+			name = "GitHubPackages"
+			url = uri("https://maven.pkg.github.com/nimbox/maven")
+			credentials {
+				username = providers.environmentVariable("GITHUB_ACTOR").orNull
+				password = providers.environmentVariable("GITHUB_TOKEN").orNull
+			}
 		}
 	}
 }
